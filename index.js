1b5d9334744d22e7d4283a5137843fa39ac93a89
Chirag Lathiya <clathiya403@gmail.com>
2024-04-16 23:29:55 +0530

    SECOND commit

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const { google } = require("googleapis");
 const fs = require("fs");
 const app = express();
+const path = require("path");
+const { dataStructured } = require("./data");
+const pathDir = path.join(__dirname, "")
 
 app.use(express.json()); // To support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // To support URL-encoded bodies
+
+
 
 const getDataFromSheet = async () => {
     const auth = new google.auth.GoogleAuth({
@@ -20,8 +25,83 @@ const getDataFromSheet = async () => {
         spreadsheetId,
         range,
     });
-
-    return response.data.values; // This will return the rows from the sheet as an array
+    const data =  response.data.values;
+    let aray = [];  
+    data.forEach((x) => {
+        let obj = {
+          Tier: x[0],
+          "Weapon Name": x[1],
+          "Weapon Type": x[2],
+          "Build Type": x[3],
+          Muzzle: x[4],
+          Barrel: x[5],
+          "Front Rail": x[6],
+          Optics: x[7],
+          Mag: x[8],
+          "Rear Grip": x[9],
+          Stock: x[10],
+          Date: x[11],
+        };
+        aray.push(obj);
+      });
+    
+      let weaponMap = new Map();
+      let tierBuildTypes = new Map();
+    
+      aray.forEach((x) => {
+        let weaponData = weaponMap.get(x["Tier"]);
+        if (!weaponData) {
+          weaponData = new Map();
+          weaponMap.set(x["Tier"], weaponData);
+          tierBuildTypes.set(x["Tier"], new Set());
+        }
+    
+        let weaponArray = weaponData.get(x["Weapon Name"]);
+        if (!weaponArray) {
+          weaponArray = [];
+          weaponData.set(x["Weapon Name"], weaponArray);
+        }
+    
+        weaponArray.push({
+          Tier: x.Tier,
+          "Weapon Name": x["Weapon Name"],
+          "Weapon Type": x["Weapon Type"],
+          "Build Type": x["Build Type"],
+          Muzzle: x["Muzzle"],
+          Barrel: x["Barrel"],
+          "Front Rail": x["Front Rail"],
+          Optics: x["Optics"],
+          Mag: x["Mag"],
+          "Rear Grip": x["Rear Grip"],
+          Stock: x["Stock"],
+          Date: x["Date"],
+        });
+    
+        tierBuildTypes.get(x["Tier"]).add(x["Build Type"]);
+      });
+    
+      let mainArray = [];
+    
+      weaponMap.forEach((tierData, tierName) => {
+        if (tierName !== "") {
+          let tierArray = [];
+          let buildTypesArray = Array.from(tierBuildTypes.get(tierName));
+          tierData.forEach((weaponArray, weaponName) => {
+            tierArray.push({
+              "Weapon Name": weaponName,
+              "Build Type": weaponArray[0]["Build Type"],
+              data: weaponArray,
+            });
+          });
+    
+          mainArray.push({
+            Tier: tierName,
+            BuildTypes: buildTypesArray,
+            data: tierArray,
+          });
+        }
+      });
+    return mainArray; // This will return the rows from the sheet as an array
 };
 
 app.get("/", async (req, res) => {
