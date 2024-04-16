86eb8aec1592a1147c010deb8f277b412631fb0d
Chirag Lathiya <clathiya403@gmail.com>
2024-04-16 18:26:39 +0530

    first commit

--- index.js
+++ index.js
@@ -1,63 +1,39 @@
 const express = require("express");
 const { google } = require("googleapis");
-const app = express();
 const fs = require("fs");
-const { dataStructured } = require("./data");
+const app = express();
 
+app.use(express.json()); // To support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // To support URL-encoded bodies
 
-app.use(express.urlencoded({ extended: true }));
-
-const getData = async() => {
+const getDataFromSheet = async () => {
     const auth = new google.auth.GoogleAuth({
-      keyFile: "credentials.json",
-      scopes: "https://www.googleapis.com/auth/spreadsheets",
+        keyFile: "credentials.json", // Ensure this file is in your project directory and gitignored
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
     });
-    const client = auth.getClient();
+    const client = await auth.getClient();
     const googleSheet = google.sheets({ version: "v4", auth: client });
     const spreadsheetId = "1jZTgOYW5rbljnEOR_WcnB_lu-ImpMi6y0m9DMCuG6Qg";
-    const metaData = await googleSheet.spreadsheets.get({ auth, spreadsheetId });
-    //   console.log(metaData);
-    const getRows = await googleSheet.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: "Web",
+    const range = "Web"; // Change as per your Google Sheet's sheet name
+
+    const response = await googleSheet.spreadsheets.values.get({
+        spreadsheetId,
+        range,
     });
-    fs.writeFile("data.json", JSON.stringify(getRows.data.values), (err) => {
-      if (err) throw err;
-      console.log("Created Json SuccessFully");
-      dataStructured()
-    });
-    //   console.log(getRows.data.values);
-}
-getData();
 
+    return response.data.values; // This will return the rows from the sheet as an array
+};
 
-// app.get("/", async (req, res) => {
-//   const auth = new google.auth.GoogleAuth({
-//     keyFile: "credentials.json",
-//     scopes: "https://www.googleapis.com/auth/spreadsheets",
-//   });
+app.get("/", async (req, res) => {
+    try {
+        const data = await getDataFromSheet();
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
-//   const client = auth.getClient();
-
-//   const googleSheet = google.sheets({ version: "v4", auth: client });
-
-//   const spreadsheetId = "1jZTgOYW5rbljnEOR_WcnB_lu-ImpMi6y0m9DMCuG6Qg";
-
-//   const metaData = await googleSheet.spreadsheets.get({ auth, spreadsheetId });
-//   //   console.log(metaData);
-//   const getRows = await googleSheet.spreadsheets.values.get({
-//     auth,
-//     spreadsheetId,
-//     range: "Web",
-//   });
-//   fs.writeFile("data.json", JSON.stringify(getRows.data.values), (err) => {
-//     if (err) throw err;
-//     console.log("Created Json SuccessFully");
-//   });
-//   //   console.log(getRows.data.values);
-// });
-
-// app.listen(5000, () => {
-//   console.log("server is running");
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
